72f042239434b19e4b3ab8ae23fb3947e3e3d96a
avinash <avinashatwork0101@gmail.com>
2025-05-03 11:45:29 +0530

    initial commit

---
+++ src/pages/landing/Landing.tsx
@@ -0,0 +1,11 @@
+import { FC } from 'react';
+import {Title} from "@mantine/core";
+
+const Landing: FC = () => {
+	return <>
+		<Title order={4}> Heljjjlo World </Title>
+	</>;
+};
+
+export default Landing
+
